79e7709f18415cbaabda7caaa1022a4e11c4b06b
Marco Carvalho <marcocarvalho.web@gmail.com>
2026-04-24 10:20:49 -0300

    Aula 24/04/2026

--- App.tsx
+++ App.tsx
@@ -27,8 +27,17 @@ export default function App() {
 
       const response = await fetch(`https://viacep.com.br/ws/${cepLimpo}/json/`);
       const data = (await response.json()) as ViaCepResponse;
-    } catch {
 
+      if(!response.ok || data.erro) {
+        setErro('CEP não encontrado');
+        return;
+      }
+
+      setCidade(data.localidade);
+    } catch {
+      setErro('Não foi possível buscar o CEP');
+    } finally {
+      setLoading(false);
     }
   }
 
